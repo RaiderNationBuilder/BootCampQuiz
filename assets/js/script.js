442230936a1f6f57dfe28fd752c844e2cc46b294
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-09-26 09:52:29 -0500

    fixed button naming issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ var startBtn = document.createElement('button', 'Start Quiz');
 var h1Content = 'Coding Quiz Challenge';
 var count = 0;
 var timeLeft = 60;
+var subtractTime = false;
 
 // var questions = ["String values must be enclosed within __________ when being assigned to variables.", "Arrays in JavaScript can be used to store __________?", "The condition in an if / else statement is enclosed with __________?",
 // "Commonly used data types DO Not Include:", "A very useful tool used during development and debugging for printing content to the debugger is:", "All Done!","High Scores"]
@@ -126,6 +127,11 @@ var screenWipe = function () {
     // var correctAnswer = addButtons();
     // console.log("correct answer " + correctAnswer);       
 // }
+var reduceclock = function() {
+    subtractTime = true; 
+    console.log(subtractTime);
+    return subtractTime;    
+}
 
 function checkClick(event) {
     screenWipe();
@@ -156,15 +162,18 @@ function checkClick(event) {
             console.log("inner for loop");
             //variables to build questionandanswer obj array iterator
             var answerIdx = 'answer' + (i + 1);
-            var answerStr = "questionsandanswers[count]." + answerIdx;
+            var answerStr = "questionsandanswers[" + count + "]." + answerIdx;
 
             //variables to build four buttons
             var BtnIdx = 'answerBtn' + (i + 1);
-            BtnIdx = $('<input type = "button" value = ' + answerStr + '/>');
+            BtnIdx = $('<input type = "button"/>');
 
             //create button
             $(BtnIdx).appendTo($('#answers'));            
-            $(BtnIdx).attr({'class': 'btn-quiz', 'id': i });            
+            $(BtnIdx).attr({'class': 'btn-quiz', 'id': i, 'value': eval(answerStr)});  
+            // $(BtnIdx).html(answerStr);
+            console.log(eval(answerStr));
+                 
         }; 
         var correctAnswer = questionsandanswers[count].correctAnswer;
     
@@ -178,34 +187,41 @@ function checkClick(event) {
             count++;                                                           
         }else if (selectedBtn != correctAnswer) {
             console.log("incorrect answer picked") 
-            $('#answers').append('<h1 class="result">Wrong</h1>')           
+            $('#answers').append('<h1 class="result">Wrong</h1>') 
+            reduceclock();          
             count++;  
         };          
     }   
     console.log(count);           
 }; 
-
-function countdown() {    
-        
+console.log(subtractTime);
+function countdown(subtractTime) {    
+    
     // quiz();
     
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    // var timeInterval = setInterval(function() {
-    //     console.log("set interval");
-    //   // As long as the `timeLeft` is greater than 0
-    //   if (timeLeft > 0) {
-    //     // Set the `textContent` of `timerEl` to show the remaining seconds
-    //     timerEl.textContent = 'Time: ' + timeLeft;       
-    //     // Decrement `timeLeft` by 1
-    //     console.log(timeLeft)
-    //     timeLeft--;      
-    //   } else {
-    //     // Use `clearInterval()` to stop the timer
-    //     clearInterval(timeInterval);
-    //     // Call the `displayMessage()` function
-    //     alert("This Quiz Has Concluded!");
-    //   }
-    // }, 1000);
+    var timeInterval = setInterval(function(subtractTime) {
+        console.log(subtractTime); 
+        // console.log("set interval");        
+        // As long as the `timeLeft` is greater than 0
+        if (timeLeft > 0) {
+            if (subtractTime) {
+                timeLeft = timeLeft - 10;
+                subtractTime = false;
+            }
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timerEl.textContent = 'Time: ' + timeLeft;       
+            // Decrement `timeLeft` by 1
+            // console.log(timeLeft)
+            timeLeft--;      
+        } else {
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            // Call the `displayMessage()` function
+            alert("This Quiz Has Concluded!");
+        }
+        
+    }, 1000);
   }
   var onLoad = function () {
     timerEl.textContent = 'Time: 0';
